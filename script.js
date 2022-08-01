4cba50567bb6bdb62e03d72b2b7c75f3e66b3cc2
Calebe <calebesantos@hotmail.com>
2022-08-01 14:31:53 -0300

    Iniciando script

---
+++ script.js
@@ -0,0 +1,10 @@
+const criarItem = (tarefa, status) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status}>
+        <div>${tarefa}</div>
+        <input type="button" value="x">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
