886a96176bf7e026db3b75bef90d1cbfac445d12
Calebe <calebesantos@hotmail.com>
2022-08-02 10:51:32 -0300

    adicionando banco e função pro banco

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//simulando um banco de dados
+let banco = [
+    {'tarefa': 'Estudar', 'status': ''},
+    {'tarefa': 'Jogar', 'status': 'checked'}
+]
+
+//função de criar item e adicionar no html, como filho da div pai.
 const criarItem = (tarefa, status) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
@@ -8,3 +15,23 @@ const criarItem = (tarefa, status) => {
     `
     document.getElementById('todoList').appendChild(item);
 }
+
+//função de limpar tarefas pra evitar duplicação ao chamar a função atualizar tela
+// enquanto existir o primeiro filho, remove o filho do todoList, removendo o ultimo filho
+const limparTarefa = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+// função que le o banco e cria um item pra cada elemento do array, que é um objeto
+const atualizarTela = () => {
+    limparTarefa();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+}
+
+atualizarTela();
+
+
+
